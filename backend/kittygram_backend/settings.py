bf0d37c8a5971a049e60b7a11382b6c2bb518228
Yaroslav <osetrov3432@gmail.com>
2026-05-24 15:00:08 +0500

    final commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -95,7 +95,9 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backend_static/'
+
+STATIC_ROOT = '/backend_static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
